2bdb5909e87b58fdfdc1629bae3abc22c53bd60b
Himal Rawal <himalrawal07@gmail.com>
2026-08-13 18:58:41 +0200

    feat: add create watermark functionality

---
+++ watermark.py
@@ -0,0 +1,11 @@
+class Watermark:
+
+# Watermark by Homoglyph
+    def _add_homoglyph_watermark(text):
+        return
+# Watermark by zerowidth
+    def _add_zerowidth_watermark(text):
+        return
+#  
+    def add_watermark(self, text):
+        return 
